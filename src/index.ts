ca834a3e5bbb0d87905fb1385c53bc51441ec761
Aman <amankrsahu003@gmail.com>
2025-08-16 20:27:02 +0530

    config(deps): initial backend project setup & configurations

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import "dotenv/config";
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import { config } from "./config/app.config";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: config.APP_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.get(
+  "/",
+  asyncHandler(async (_req: Request, res: Response, _next: NextFunction) => {
+    res.status(HTTPSTATUS.OK).json({
+      message: "Server is running",
+    });
+  })
+);
+
+app.use(errorHandler);
+
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+});
